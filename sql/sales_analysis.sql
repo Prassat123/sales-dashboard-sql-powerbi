6820f7ec547117dc46a5bdfcf30d82b3e95a8847
Prassat Mishra <prassatmishra@gmail.com>
2026-06-10 12:06:37 -0500

    Update sales_analysis.sql

--- sql/sales_analysis.sql
+++ sql/sales_analysis.sql
@@ -1,1 +1,15 @@
+-- View all data
+SELECT *
+FROM sales_data;
 
+-- Total Sales
+SELECT SUM(sales) AS total_sales
+FROM sales_data;
+
+-- Total Orders
+SELECT COUNT(order_id) AS total_orders
+FROM sales_data;
+
+-- Average Sales
+SELECT AVG(sales) AS average_sales
+FROM sales_data;
